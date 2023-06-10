6567a5a4cffe194656984e8681d3a482e4195802
Zoltan Laszlo Kiss <laszlo.zoltan.kiss@gmail.com>
2023-06-10 18:10:37 +0200

    Fix

--- variables.tf
+++ variables.tf
@@ -6,6 +6,5 @@ variable "name" {
 variable "keep_locally" {
     description = "If true, then the Docker image won't be deleted on destroy operation. If this is false, it will delete the image from the docker local storage on destroy operation."
     type        = bool
-    # What is the default here?
     default     = true
 }
