20f3128d784d240cec4da6b524240099627fd536
Zoltan L. Kiss <laszlo.zoltan.kiss@gmail.com>
2023-06-10 16:58:16 +0000

    Initial version with basic functionalities

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "docker_image" "this" {
+    name         = var.name
+    keep_locally = var.keep_locally
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+    description = "The name of the Docker image, including any tags or SHA256 repo digests."
+    type        = string
+}
+
+variable "keep_locally" {
+    description = "If true, then the Docker image won't be deleted on destroy operation. If this is false, it will delete the image from the docker local storage on destroy operation."
+    type        = bool
+    default     = true
+}
